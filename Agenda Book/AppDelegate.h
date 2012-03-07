5bffb6ef2bf3abaca93eb3cd81f343ea1e4b9b95
Matt Bilker <me@mbilker.us>
2012-03-06 20:12:57 -0500

    Initial Commit

---
+++ Agenda Book/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Agenda Book
+//
+//  Created by Matt Bilker on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Agenda Book/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Agenda Book
+//
+//  Created by Matt Bilker on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
